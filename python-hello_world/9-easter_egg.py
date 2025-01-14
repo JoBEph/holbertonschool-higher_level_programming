777a663a44a364d8c6b4a0d64d88f576d301d977
root <root@JordanPro.localdomain>
2025-01-14 15:36:04 +0100

    easter egg

--- python-hello_world/9-easter_egg.py
+++ python-hello_world/9-easter_egg.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-str = """
-The Zen of Python, by Tim Peters
-
-Beautiful is better than ugly.
+print('The Zen of Python, by Tim Peters\n')
+print('''Beautiful is better than ugly.
 Explicit is better than implicit.
 Simple is better than complex.
 Complex is better than complicated.
@@ -20,6 +18,4 @@ Now is better than never.
 Although never is often better than *right* now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!
-"""
-print(str)
+Namespaces are one honking great idea -- let's do more of those!''')
