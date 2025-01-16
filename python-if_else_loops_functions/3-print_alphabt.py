3343307261a0bf1f6f9004e45ae46d38c25d9465
root <root@JordanPro.localdomain>
2025-01-16 10:49:19 +0100

    print alphabet whithout q and e

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-kicker = {'q', 'e'}
-for alphabet in range(ord('a'), ord('z') + 1):
-    if chr(alphabet) not in kicker:
-         print(chr(alphabet), end="")
+for alphabet in range(97, 123):
+         if alphabet != 101 and alphabet != 113:
+            print("{:c}".format(alphabet), end="")
