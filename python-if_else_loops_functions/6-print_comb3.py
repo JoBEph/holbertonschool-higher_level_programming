7a477600487119e2ef93d7857851ea91577ec8a8
root <root@JordanPro.localdomain>
2025-01-16 13:17:21 +0100

    pint comb

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for x in range(10):
     for y in range(x + 1, 10):
-        if x < 9 or y < 9:
-            print("{:02d}, ".format(x * 10 + y), end='')
-print("{:02d}".format(89))
+        if x == 8 or y == 9:
+            print("{:d}{:d}".format(x, y))
+        else:
+            print("{:d}{:d}".format(x, y), end=", ")
