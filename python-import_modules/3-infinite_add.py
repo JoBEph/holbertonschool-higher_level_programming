738dba9bf9d93f6224bb2388059b1c339f1bd94d
root <root@JordanPro.localdomain>
2025-01-17 13:00:21 +0100

    infinite add

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 if __name__ == "__main__":
     import sys
+    total = 0
     args = sys.argv[1:]
-    total = sum([int(arg) for arg in args])
+    total += int(args[0])
     print(total)
