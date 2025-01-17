53225ecb96d83aa395fee395491b715cdbb7b6b7
root <root@JordanPro.localdomain>
2025-01-17 13:09:44 +0100

    infinit add

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 if __name__ == "__main__":
     import sys
-    
+    argv = sys.argv[1:]
+    argc = len(argv)
     result = 0
-    for arg in sys.argv[1:]
-    result += int(arg)
+    for arg in argv:
+        result += int(arg)
     print(result)
+    
