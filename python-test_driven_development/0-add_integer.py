bd3acbfd3f9df5a2f3d961b2da5956adf41b7f53
root <root@JordanPro.localdomain>
2025-01-24 16:05:48 +0100

    function that adds 2 integers

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
-
+"""
+add two integers module
+"""
 def add_integer(a, b=98):
 
+    """
+    add_integer: add two integers
+    a: first integer
+    b: second integer
+    return: int a and b
+    """
+    
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
