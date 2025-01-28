355ca924f25cd5986236eaba8c4ef9be642d2c98
root <root@JordanPro.localdomain>
2025-01-28 10:36:56 +0100

    class Square that defines a square by property .setter

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -10,18 +10,24 @@ class Square:
     typeserror and valueerror and define the area
     """
 
-    @property
     def __init__(self, size=0):
         self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-        @size.setter
-        def area(self):
-            """
-            calculate area
-            """
-            return self.__size ** 2
+    def area(self):
+        """
+        calculate area
+        """
+        return self.__size ** 2
