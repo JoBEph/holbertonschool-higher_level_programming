80993307c5a17aa320bc6651affc9b639c54eb2d
root <root@JordanPro.localdomain>
2025-01-28 10:05:37 +0100

    class Square that defines a square by with def area

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
 
+"""
+this is square module
+"""
+
+
 class Square:
+    """
+    typeserror and valueerror and define the area
+    """
+
     def __init__(self, size=0):
         self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size <0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         return self.__size ** 2
