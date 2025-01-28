7cf5ca23649ef3287803f3fbc96ed69b5856658c
root <root@JordanPro.localdomain>
2025-01-28 10:39:25 +0100

    add def my print

---
+++ python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+"""
+this is square module
+"""
+
+
+class Square:
+    """
+    typeserror and valueerror and define the area
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        calculate area
+        """
+        return self.__size ** 2
+    
+    def my_print(self):
+        """
+        print square
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
