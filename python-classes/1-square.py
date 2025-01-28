fa25c5264c3ef8056d3bab963bb0a03f341f4c2e
root <root@JordanPro.localdomain>
2025-01-28 09:57:16 +0100

    class Square that defines a square by size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 
+"""
+this is square module
+"""
+
+
 class Square:
+    """
+    create classe named square
+    """
+
     def __init__(self, size):
         self.__size = size
