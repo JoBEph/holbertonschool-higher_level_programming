7e0a5d411e47d436800a7ad50b27aee506eb30fd
root <root@JordanPro.localdomain>
2025-01-28 10:00:40 +0100

    class Square that defines a square bysize to 0 and typeerror and valuerror

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,6 +4,7 @@
 this is square module
 """
 
+
 class Square:
     """
     create classe named square with typeserror and valueerror
@@ -13,6 +14,6 @@ class Square:
         self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size <0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
