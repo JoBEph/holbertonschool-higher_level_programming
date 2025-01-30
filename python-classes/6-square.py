5845f7a96c4bc4b8b81edaae743938adf84a6fd0
root <root@JordanPro.localdomain>
2025-01-30 11:35:57 +0100

    Coordinates of a square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -36,7 +36,7 @@ class Square:
         if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         if not all(isinstance(i, int) for i in value):
-            raise TypeError("check every element of position")
+            raise TypeError("position must be a tuple of 2 positive integers")
         if value[0] < 0 or value[1] < 0:
             raise ValueError("position must be >= 0")
         self.__position = value
