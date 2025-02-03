1da059bd8346e0d8b0d6fb0883edb7fcc66b9e0f
root <root@JordanPro.localdomain>
2025-02-03 13:19:43 +0100

    Rectangle

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""create empty class base geometry"""
+
+
+class BaseGeometry:
+    """empty class base geo"""
+
+    def area(self):
+
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+
+        """check if value is int > 0"""
+
+        if type(value) is not int:
+            raise TypeError(f'{name} must be an integer')
+        if value <= 0:
+            raise ValueError(f'{name} must be greater than 0')
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle from base geometry"""
+
+    def __init__(self, width, height):
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
