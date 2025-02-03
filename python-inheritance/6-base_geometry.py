136085405dfe08eb9a70505242b7230468f19df4
root <root@JordanPro.localdomain>
2025-02-03 12:57:46 +0100

    Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""create empty class base geometry"""
+
+
+class BaseGeometry:
+    """empty class base geo"""
+
+
+def area(self):
+
+    raise Exception('area() is not implemented')
