ce897441493f2a5bec786f17f170a0e7aa7928a6
root <root@JordanPro.localdomain>
2025-02-03 09:39:50 +0100

    Abstract Animal Class and its Subclasses

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -2,13 +2,19 @@
 
 from abc import ABC, abstractmethod
 
+
 class Animal(ABC):
 
     @abstractmethod
     def sound(self):
         pass
-    
-    def dog(self):
-        self.sound("bark")
-    def cat(self):
-        self.sound("meow")
+
+
+class Dog(Animal):
+    def sound(self):
+        return "bark"
+
+
+class Cat(Animal):
+    def sound(self):
+        return "meow"
