213d5dbe23e12b1b0d820db2726b613451c2202f
root <root@JordanPro.localdomain>
2025-02-03 12:24:40 +0100

    Same class or inherit from

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+contain the function is_kind_of_class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """return true or false if the object is an instance"""
+    return isinstance(obj, a_class)
