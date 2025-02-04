380d6b18db45fd2227856ea0a35f82d769426125
root <root@JordanPro.>
2025-02-04 09:08:48 +0100

    fix changes

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -7,15 +7,11 @@ class BaseGeometry:
     """empty class base geo"""
 
     def area(self):
-
         """area self"""
-
         raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-
         """check if value is int > 0"""
-
         if type(value) is not int:
             raise TypeError(f'{name} must be an integer')
         if value <= 0:
@@ -26,19 +22,16 @@ class Rectangle(BaseGeometry):
     """Rectangle from base geometry"""
 
     def __init__(self, width, height):
-
         """init self, width, height for rectangle"""
-
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def area(self):
-        """area self for rectangle class"""
-
-        return self.width * self.height
+        """return area of the rectangle"""
+        return self.__width * self.__height
 
     def __str__(self):
-        """return str"""
-        return (f'[Rectangle] {self.__width}/ {self.__height}')
+        """return string representation of the rectangle"""
+        return f"[Rectangle] {self.__width}/{self.__height}"
