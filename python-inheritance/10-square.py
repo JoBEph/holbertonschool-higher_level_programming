14977eabdce96f6b71067dd63daeac2d9a347c1c
root <root@JordanPro.>
2025-02-04 09:24:09 +0100

    Square #1

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -41,10 +41,10 @@ class Square(BaseGeometry):
     """Square from Base geometry"""
 
     def __init__(self, size):
-
+        """private size and validate integer"""
         self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
-
+        """define area"""
         return self.__size * self.__size
