90918f56d488841245c3ec02b8709efef8dd6730
root <root@JordanPro.>
2025-02-10 17:37:30 +0100

    save to jason file

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""fonction that directly serialize in python object"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
