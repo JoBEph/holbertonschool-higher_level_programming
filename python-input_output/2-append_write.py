8b9de719f10c2efd9d10fe4d035d45e7dc9b2f79
root <root@JordanPro.>
2025-02-10 17:08:12 +0100

    append write

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""overite file from txt and return the number of characters"""
+
+
+def append_write(filename="", text=""):
+
+    with open(filename, "a", encoding="utf-8") as f:
+        data_file = f.write(text)
+        return (data_file)
