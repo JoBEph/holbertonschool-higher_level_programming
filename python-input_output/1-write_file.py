5f57660d18d7db588d837a0407d1639d8c1cd437
root <root@JordanPro.>
2025-02-10 16:59:50 +0100

    update write file with pycodestyle

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""overite file from txt and return the number of characters"""
+
 
 def write_file(filename="", text=""):
+
     with open(filename, "w", encoding="utf-8") as f:
         data_file = f.write(text)
         return (data_file)
