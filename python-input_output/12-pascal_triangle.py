4310fb6dc07326c89299ff6b42abf2ecfc8787ca
root <root@JordanPro.>
2025-02-12 12:00:17 +0100

    pascal triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """pascal triangle"""
 
+
 def pascal_triangle(n):
     """def pascal triangle"""
     if n <= 0:
-        return ""
-    
+        return []
+
     triangle = []
-    
+
     for i in range(n):
         row = [1] * (i + 1)
     for j in range(1, i):
         row[j] = triangle[i-1][j-1] + triangle[i-1][j]
-    
+
     triangle.append(row)
     print(row)
