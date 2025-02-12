d3b5b48b2f840177bd7709dc5db2ee4a0e749aa5
root <root@JordanPro.>
2025-02-12 16:00:17 +0100

    Serialize/Deserialize ditionnary from xml file

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -2,10 +2,11 @@
 
 import xml.etree.ElementTree as ET
 
-def serialize_to_xml(dictionary, filename):
 
-    root = ET.element("data")
-    
+def serialize_to_xml(dictionary, filename):
+    """Serialize a dictionary to an XML file."""
+    root = ET.Element("data")
+
     for key, value in dictionary.items():
         item = ET.SubElement(root, key)
         item.text = str(value)
@@ -13,7 +14,14 @@ def serialize_to_xml(dictionary, filename):
     tree = ET.ElementTree(root)
     tree.write(filename, encoding='utf-8', xml_declaration=True)
 
-def deserialize_from_xml(filename):
 
+def deserialize_from_xml(filename):
+    """Deserialize an XML file to a dictionary."""
     tree = ET.parse(filename)
     root = tree.getroot()
+
+    dictionary = {}
+    for child in root:
+        dictionary[child.tag] = child.text
+
+    return dictionary
