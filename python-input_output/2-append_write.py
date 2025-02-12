591e77751c919797aa367a89b63c7c699d79fee7
root <root@JordanPro.>
2025-02-12 10:59:20 +0100

    append write

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,7 +3,9 @@
 
 
 def append_write(filename="", text=""):
+    """def append write"""
 
     with open(filename, "a", encoding="utf-8") as f:
+        """with open"""
         data_file = f.write(text)
-        return (data_file)
+        return data_file
