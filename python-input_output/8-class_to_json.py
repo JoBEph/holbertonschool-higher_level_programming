82618aee0ece42e422782dd060039c3177816523
root <root@JordanPro.>
2025-02-12 10:52:54 +0100

    correct 8class to json

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON"""
-import json
+"""function that returns the dictionary"""
 
 
 def class_to_json(obj):
+    """def class json"""
 
     return obj.__dict__
