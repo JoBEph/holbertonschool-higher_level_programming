e2bc4bdaf4a19683f1fbe3f9c9fa03fad7ce45b0
root <root@JordanPro.>
2025-02-12 10:58:10 +0100

    correct pycodestyle

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,7 +3,9 @@
 
 
 def write_file(filename="", text=""):
+    """def write file"""
 
     with open(filename, "w", encoding="utf-8") as f:
+        """with open"""
         data_file = f.write(text)
-        return (data_file)
+        return data_file
