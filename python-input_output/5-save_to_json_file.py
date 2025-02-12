d21829b94d6d62d25c4aec7be15e4affd20d8d5e
root <root@JordanPro.>
2025-02-12 11:04:15 +0100

    correct 5save json

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -4,5 +4,7 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """def save to json file"""
     with open(filename, "w", encoding="utf-8") as f:
+        """with open"""
         json.dump(my_obj, f)
