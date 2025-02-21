cf17024af82f21c490aa78c524356dd4edc7c6fe
root <root@JordanPro.>
2025-02-21 21:39:47 +0100

    request

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,33 +1,40 @@
-#!/usr/bin/env python3
-"""basic Python script to fetch posts from JSONPlacehold"""
+#!/usr/bin/python3
 
 import requests
 import csv
 
 
 def fetch_and_print_posts():
-    url = 'https://jsonplaceholder.typicode.com/posts'
+
+    url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(url)
-    print(f'status_code: {response.status_code}')
+    print(f"Status Code: {response.status_code}")
 
     if response.status_code == 200:
         posts = response.json()
         for post in posts:
-            print(post['title'])
+            print(post["title"])
 
 
 def fetch_and_save_posts():
-    url = 'https://jsonplaceholder.typicode.com/posts'
+
+    url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(url)
+
     status_code = response.status_code
     posts = response.json()
     data = [
-        {'id': post['id'], 'title': post['title'], 'body': post['body']}
-        for post in posts
-    ]
-    with open('posts.csv', 'w', newline="", encoding="utf-8") as fcsv:
-        writer = csv.DictWriter(fcsv, fieldnames=['id', 'title', 'body'])
+        {
+            "id": post["id"],
+            "title": post["title"],
+            "body": post["body"]
+        }
+        for post in posts]
+
+    with open("posts.csv", "w", newline="", encoding="utf-8") as csvfile:
+        fieldnames = ["id", "title", "body"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         writer.writerows(data)
 
-    print('posts have been saved to posts.csv')
+    print("Posts have been saved to post.csv")
