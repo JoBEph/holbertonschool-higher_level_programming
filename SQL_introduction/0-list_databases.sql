57e5ffd2202426917418ce79ca1ef95bacc3f6a1
JoBEph <jordan.baroux31@gmail.com>
2025-03-03 11:18:08 +0100

    list of database

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,10 +1,4 @@
-Enter password: 
-Database                                                                                     
-information_schema                                                                           
-mysql                                                                                        
-performance_schema                                                                           
-sys
 -- 0-list_databases.sql
 -- Lists all databases of a MySQL server.
-
+--
 SHOW DATABASES;
