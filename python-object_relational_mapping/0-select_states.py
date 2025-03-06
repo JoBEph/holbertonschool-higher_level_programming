9c319f16f28e2340e8f6dc719797d16e267fd189
JoBEph <jordan.baroux31@gmail.com>
2025-03-06 15:07:58 +0100

    add script to select and display states from the database

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    def get_states():
+        db = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306
+        )
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+        states = cursor.fetchall()
+        
+        for row in states:
+            print(row)
+        cursor.close()
+        db.close()
+
+    get_states()
