bca69d610d5b1dcc09c9a9b20b313ef9f2e604ae
JoBEph <jordan.baroux31@gmail.com>
2025-03-06 10:00:14 +0100

    title comedy only

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -4,7 +4,7 @@
 -- Display: tv_shows.title
 -- Sorted in ascending order by the show title
 --
-SELECT tv_genres.title
+SELECT tv_shows.title
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
