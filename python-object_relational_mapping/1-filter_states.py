80e6eec77d56d304afd4011067922fe5bd309b20
JoBEph <jordan.baroux31@gmail.com>
2025-03-07 11:25:25 +0100

    fix: use BINARY keyword in SQL query to ensure case-sensitive matching for state names

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -20,7 +20,10 @@ if __name__ == "__main__":
             port=3306
             )
         cursor = db.cursor()
-        query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+        query = query = (
+            "SELECT * FROM states "
+            "WHERE BINARY name LIKE 'N%' ORDER BY id ASC"
+        )
         cursor.execute(query)
         states = cursor.fetchall()
         for state in states:
