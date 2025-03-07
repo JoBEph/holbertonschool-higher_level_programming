8f2bb1d650ad0d1e984eec9f9d357d05f5d60680
JoBEph <jordan.baroux31@gmail.com>
2025-03-07 14:07:40 +0100

    filter citites

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -14,12 +14,12 @@ if __name__ == "__main__":
         port=3306,
     )
     cursor = db.cursor()
-    cursor.execute("SELECT cities.id, cities.name, states.name \
-                   FROM cities JOIN states ON cities.state_id = states.id \
-                   WHERE states.name = %s ORDER BY id ASC", (sys.argv[4],))
+    cursor.execute("SELECT cities.name FROM cities \
+    JOIN states ON cities.state_id = states.id \
+    WHERE states.name = %s ORDER BY cities.id ASC", (sys.argv[4],),)
     rows = cursor.fetchall()
-    for row in rows:
-        print(row)
+    city_names = [row[0] for row in rows]
+    print(", ".join(city_names))
 
     cursor.close()
     db.close()
