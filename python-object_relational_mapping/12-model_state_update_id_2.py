e9e5c64e301998cc84cb8f76755ccd35b149843f
JoBEph <jordan.baroux31@gmail.com>
2025-03-07 18:09:20 +0100

    model state udapte by id

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-""" Changes the name of a State object from the database hbtn_0e_6_usa """
+""" Changes the name of a State object from the database hbtn_0e_6_usa by id 2 """
 
 import sys
 from model_state import State, Base
