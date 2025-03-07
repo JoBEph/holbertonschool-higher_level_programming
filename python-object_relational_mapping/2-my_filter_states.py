b5ad9e4e3021dc11f6d0e5cd8b4165f137617d97
JoBEph <jordan.baroux31@gmail.com>
2025-03-07 12:35:59 +0100

    filter states Arizona

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,22 +1,21 @@
-#!/usr/bin/python3
-
+#!/usr/bin/env python3
+""" filter list"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-
     db = MySQLdb.connect(
         host="localhost",
         user=sys.argv[1],
-        password=sys.argv[2],
+        passwd=sys.argv[2],
         db=sys.argv[3],
         charset="utf8",
         port=3306
-        )
+    )
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states WHERE name \
-            LIKE BINARY '{}%' ORDER BY id ASC".format(sys.argv[4]))
+    LIKE BINARY '{}%' ORDER BY id ASC".format(sys.argv[4]))
     rows = cur.fetchall()
 
     for row in rows:
