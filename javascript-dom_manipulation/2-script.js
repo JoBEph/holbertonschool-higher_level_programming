f8104153586ae15f0fc297d29604bdf995979d55
JoBEph <jordan.baroux31@gmail.com>
2025-04-02 11:09:09 +0200

    remove style color to add red class

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+const red_header = document.querySelector('#red_header')
+const header = document.querySelector('header')
+red_header.addEventListener('click', function() {
+    header.classList.add('red')
+});
